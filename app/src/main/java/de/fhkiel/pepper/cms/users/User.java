84f6ee93eef0b67d6c94042fd146ae0c035e3bd9
Hannes Eilers <hannes.eilers@fh-kiel.de>
2020-09-14 13:32:35 +0200

    Updated test app

--- app/src/main/java/de/fhkiel/pepper/cms/users/User.java
+++ app/src/main/java/de/fhkiel/pepper/cms/users/User.java
@@ -7,6 +7,7 @@ public class User {
     public String salut = "";
     public String prename = "";
     public String lastname = "";
+    // TODO birthday
     public HashMap<String, JSONObject> gamedata;    // String is game name, JSONObject ist game settings as json
 
     public String getUsername(){
