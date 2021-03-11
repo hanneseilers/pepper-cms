8da0ed1df97716f929934aafeff7ef67c074b3ef
Hannes Eilers <mail@hanneseilers.de>
2021-03-11 19:23:07 +0100

    Added admin site overview

---
+++ app/src/main/java/de/fhkiel/pepper/cms/admin/AdminFragmentOverview.java
@@ -0,0 +1,13 @@
+package de.fhkiel.pepper.cms.admin;
+
+import androidx.fragment.app.Fragment;
+
+import de.fhkiel.pepper.cms.R;
+
+public class AdminFragmentOverview extends Fragment {
+
+    public AdminFragmentOverview(){
+        super(R.layout.admin_fragment_overview);
+    }
+
+}
